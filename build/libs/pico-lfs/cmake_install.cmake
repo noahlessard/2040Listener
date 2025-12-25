d1f2a7ed2599967753beddeb12dde80a2b05f79c
Noah Lessard <24837219+noahlessard@users.noreply.github.com>
2025-12-25 01:08:10 -0600

    commit littlefs

--- build/libs/pico-lfs/cmake_install.cmake
+++ build/libs/pico-lfs/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Lindsay/Documents/2040Listener
+# Install script for directory: C:/Users/Lindsay/Documents/2040Listener/libs/pico-lfs
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,33 +37,9 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Users/Lindsay/.pico-sdk/toolchain/14_2_Rel1/bin/arm-none-eabi-objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/Lindsay/Documents/2040Listener/build/pico-sdk/cmake_install.cmake")
-  include("C:/Users/Lindsay/Documents/2040Listener/build/pico_pio_usb/cmake_install.cmake")
-  include("C:/Users/Lindsay/Documents/2040Listener/build/libs/pico-lfs/cmake_install.cmake")
-
-endif()
-
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/Lindsay/Documents/2040Listener/build/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
-if(CMAKE_INSTALL_COMPONENT)
-  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-  else()
-    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
-    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
-    unset(CMAKE_INST_COMP_HASH)
-  endif()
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/Lindsay/Documents/2040Listener/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/Lindsay/Documents/2040Listener/build/libs/pico-lfs/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
